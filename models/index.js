e963ce5e5679577fab95caf6ba8f25e3f172c5d3
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-04-18 19:45:13 -0300

    did the models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey:'',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  foreignKey: '',
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  foreignKey: '',
+})
 
 module.exports = {
   Product,
